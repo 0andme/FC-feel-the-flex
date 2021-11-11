e7ab4b9b4e539ba24e8d43086dbce6150f9edc4f
0andme <yu00mi97@gmail.com>
2021-11-12 01:22:46 +0900

    feat : App.vue 추가 및 전역 폰트 설정
    - body에 전역 폰트 설정

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <TheHeader />
+  <RouterView />
+</template>
+
+<script>
+import TheHeader from '~/components/TheHeader'
+
+export default {
+  components: {
+    TheHeader
+  }
+}
+</script>
+<style lang="scss">
+body{
+  font-family: 'S-CoreDream-4Regular';
+;
+}
+</style>
