dedab0a98d90f2eca970c4c2bddd5153181215fa
0andme <yu00mi97@gmail.com>
2021-11-12 00:56:00 +0900

    feat : 인증 api 공통 url과 api 공통 헤더 파일 추가 - 모든 인증 api함수에서 사용되는 url 추가 - 모든 api 함수에서 사용되는 헤더 추가

---
+++ src/utils/apiInfo.js
@@ -0,0 +1,6 @@
+export const AUTH_BASE_URL='https://asia-northeast3-heropy-api.cloudfunctions.net/api/auth'
+export const HEADERS={
+  'content-type': 'application/json',
+  'apikey': 'FcKdtJs202110',
+  'username': 'flex'
+}
