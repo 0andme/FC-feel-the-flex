449ed50da5ad3150875c0990bf47940b6d51278c
0andme <yu00mi97@gmail.com>
2021-11-15 14:12:14 +0900

    feat : 계좌 api 요청 주소 추가

--- src/utils/apiInfo.js
+++ src/utils/apiInfo.js
@@ -1,4 +1,5 @@
 export const AUTH_BASE_URL='https://asia-northeast3-heropy-api.cloudfunctions.net/api/auth'
+export const BANK_BASE_URL='https://asia-northeast3-heropy-api.cloudfunctions.net/api/account'
 export const HEADERS={
   'content-type': 'application/json',
   'apikey': 'FcKdtJs202110',
